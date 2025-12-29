85b2684e4fae9f7ac6acca54dc1c2d1c9f8e453d
Djordje Jovanovic <dj.jovanovic6@gmail.com>
2025-12-29 20:21:41 +0100

    Save local changes before merge

--- employee-management-system-f/src/App.jsx
+++ employee-management-system-f/src/App.jsx
@@ -1,8 +1,13 @@
 import Landing_page from "./pages/Landing-page/Landing_page.jsx";
+import SignIn from "./pages/Sign_in/Sign_in.jsx";
+import { Route, Routes } from "react-router-dom";
 export default function App() {
   return (
-    <div className="min-h-screen bg-white">
-      <Landing_page />
-    </div>
+    <Routes>
+   
+      <Route path="/" element={<Landing_page />} />
+      <Route path="/signin" element={<SignIn />} />
+    
+    </Routes>
   );
 }
---
+++ employee-management-system-f/src/pages/Sign_in/components/Sign_in_header.jsx
@@ -0,0 +1,8 @@
+export default function Sing_in_header() {
+    return (
+        <header className="flex flex-row items-center">
+                <img src="../../../public/images/logo.webp" alt="Logo" className="w-32 h-auto mb-4" />
+                <h1 className="text-black text-3xl">EmployeeHub</h1>
+        </header>
+    );
+}
